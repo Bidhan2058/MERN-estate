7db86603223b8ac0c49f66b89adea67b3c55f00c
Bidhan2058 <59955603+Bidhan2058@users.noreply.github.com>
2024-05-10 14:40:04 +0545

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
-import SignOut from "./pages/SignOut";
+import Header from "./compoents/Header";
+import SignIn from "./pages/SignIn";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="about" element={<About/>} />
-        <Route path="profile" element={<Profile/>} />
-        <Route path="sign-in" element={<SignUp/>} />
-        <Route path="sign-out" element={<SignOut/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
   );
