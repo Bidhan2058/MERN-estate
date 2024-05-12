fc232ea878103d2fac8b6dc9010a630187fa7f03
Bidhan2058 <59955603+Bidhan2058@users.noreply.github.com>
2024-05-12 18:42:46 +0545

    signin api

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,7 @@
-const errorHandeler = (statusCode, message) => {
+const errorHandler = (statusCode, message) => {
   const error = new Error();
   error.statusCode = statusCode;
   error.message = message;
   return error;
 };
-module.exports = errorHandeler;
+module.exports = errorHandler;
