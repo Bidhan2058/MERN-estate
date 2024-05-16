f0fcc3ed405eb4ea3786c92ef10af9b503509556
Bidhan2058 <59955603+Bidhan2058@users.noreply.github.com>
2024-05-16 12:52:01 +0545

    creating listing model, api, and saving in db

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
+const listingRouter = require("./routes/listing.route");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 dotenv.config();
@@ -15,7 +16,6 @@ mongoose
     console.log(err);
   });
 
-
 const app = express();
 
 app.use(express.json());
@@ -26,7 +26,7 @@ app.listen(3000, () => console.log("listening"));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-
+app.use("/api/listing", listingRouter);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
